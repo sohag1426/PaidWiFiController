c8d7c6943ed56e2c67fd42689701732bb87eea77
Md Sohag Hosen <sohag1426@gmail.com>
2022-04-20 11:47:52 +0600

    sms gateway controller updated

---
+++ app/Console/Commands/RadSqlRelay.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Freeradius\radacct;
+use App\Models\pgsql\pgsql_customer;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class RadSqlRelay extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rad:sql_relay {debug=0}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'SQL Log to SQL Database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $debug = $this->argument('debug');
+
+        $timestamp = Carbon::now()->timestamp;
+
+        // db connection
+        $password = config('database.connections.central.password');
+
+        // Start Accounting-Request
+        if (file_exists('/var/log/freeradius/acct_start_raw.sql')) {
+
+            $acct_start_file = '/var/log/freeradius/acct_start_' . $timestamp . '.sql';
+
+            // copy and truncate accounting start log
+            shell_exec("cp -f /var/log/freeradius/acct_start_raw.sql $acct_start_file");
+            shell_exec('echo -n "" > /var/log/freeradius/acct_start_raw.sql');
+
+            // start processing
+            shell_exec("mysql --user=ispbillingone --password=$password --database=ispbillingone  < $acct_start_file");
+            shell_exec("rm $acct_start_file");
+        }
+
+        // Interim-Update Accounting-Request
+        if (file_exists('/var/log/freeradius/acct_interim_update_raw.sql')) {
+
+            $interim_update_file = '/var/log/freeradius/intrim_update_' . $timestamp . '.sql';
+
+            // copy and truncate acct_interim_update log
+            shell_exec("cp -f /var/log/freeradius/acct_interim_update_raw.sql $interim_update_file");
+            shell_exec('echo -n "" > /var/log/freeradius/acct_interim_update_raw.sql');
+
+            // compress request
+            $lines = file($interim_update_file);
+            $trimed_lines = [];
+            foreach ($lines as $line) {
+                $array = explode('WHERE', $line);
+                $array_2 = explode('AND', $array[1]);
+                $array_3 = explode('=', $array_2[0]);
+                $username = trim($array_3[1]);
+                $trimed_lines[$username] = $line;
+            }
+
+            // process trimed_lines
+            foreach ($trimed_lines as $username => $trimed_line) {
+
+                $username = Str::replace("'", "", $username);
+
+                $update_string = Str::between($trimed_line, 'SET', 'WHERE');
+
+                $update_statement_array = explode(',', $update_string);
+
+                $updates = [];
+
+                foreach ($update_statement_array as $update_statement) {
+                    $key_value_array =  explode("=", $update_statement);
+                    if (count($key_value_array) == 2) {
+                        $updates[trim($key_value_array['0'])] = trim($key_value_array['1']);
+                    }
+                }
+
+                $keys_found = array_keys($updates);
+
+                $expected_keys = [
+                    'acctsessionid',
+                    'acctuniqueid',
+                    'nasipaddress',
+                    'callingstationid',
+                    'framedipaddress',
+                    'nasidentifier',
+                    'acctupdatetime',
+                    'acctsessiontime',
+                    'acctinputoctets',
+                    'acctoutputoctets'
+                ];
+
+                $key_diff = array_diff($expected_keys, $keys_found);
+
+                if (count($key_diff)) {
+                    if ($debug) {
+                        $this->info('Not Updating: ' . $username);
+                        print_r($key_diff);
+                    }
+                    continue;
+                }
+
+                $sql_update = [
+                    'acctsessionid' => Str::replace("'", "", $updates['acctsessionid']),
+                    'acctuniqueid' => Str::replace("'", "", $updates['acctuniqueid']),
+                    'nasipaddress' => Str::replace("'", "", $updates['nasipaddress']),
+                    'callingstationid' => Str::replace("'", "", $updates['callingstationid']),
+                    'framedipaddress' => Str::replace("'", "", $updates['framedipaddress']),
+                    'nasidentifier' => Str::replace("'", "", $updates['nasidentifier']),
+                    'acctupdatetime' => DB::raw($updates['acctupdatetime']),
+                    'acctsessiontime' => $updates['acctsessiontime'],
+                    'acctinputoctets' => DB::raw(Str::replace("'", "", $updates['acctinputoctets'])),
+                    'acctoutputoctets' => DB::raw(Str::replace("'", "", $updates['acctoutputoctets'])),
+                ];
+
+                $affected = radacct::where('username', $username)
+                    ->whereNull('acctstoptime')
+                    ->update([
+                        'nasidentifier' => $sql_update['nasidentifier'],
+                        'acctupdatetime' => $sql_update['acctupdatetime'],
+                        'acctsessiontime' => $sql_update['acctsessiontime'],
+                        'acctinputoctets' => $sql_update['acctinputoctets'],
+                        'acctoutputoctets' => $sql_update['acctoutputoctets'],
+                    ]);
+
+                if ($affected == 0) {
+
+                    $customer = pgsql_customer::where('username', $username)->first();
+
+                    if ($customer) {
+                        $radacct = new radacct();
+                        $radacct->mgid = $customer->mgid;
+                        $radacct->operator_id = $customer->operator_id;
+                        $radacct->acctsessionid = $sql_update['acctsessionid'];
+                        $radacct->acctuniqueid = $sql_update['acctuniqueid'];
+                        $radacct->username = $customer->username;
+                        $radacct->nasipaddress = $sql_update['nasipaddress'];
+                        $radacct->callingstationid = $sql_update['callingstationid'];
+                        $radacct->acctterminatecause = "null";
+                        $radacct->framedipaddress = $sql_update['framedipaddress'];
+                        $radacct->nasidentifier = $sql_update['nasidentifier'];
+                        $radacct->acctstarttime = Carbon::now();
+                        $radacct->acctupdatetime = Carbon::now();
+                        $radacct->acctsessiontime = $sql_update['acctsessiontime'];
+                        $radacct->acctinputoctets = $sql_update['acctinputoctets'];
+                        $radacct->acctoutputoctets = $sql_update['acctoutputoctets'];
+                        $radacct->save();
+
+                        if ($debug) {
+                            $this->info('New radacct row created. Customer ID: ' . $customer->id . " mgid : " . $customer->mgid);
+                        }
+                    }
+                }
+            }
+
+            shell_exec("rm $interim_update_file");
+        }
+
+        // Stop Accounting-Request
+        if (file_exists('/var/log/freeradius/acct_stop_raw.sql')) {
+
+            $acct_stop_file = '/var/log/freeradius/acct_stop_' . $timestamp . '.sql';
+
+            // copy and truncate accounting Stop log
+            shell_exec("cp -f /var/log/freeradius/acct_stop_raw.sql $acct_stop_file");
+            shell_exec('echo -n "" > /var/log/freeradius/acct_stop_raw.sql');
+
+            // start processing
+            shell_exec("mysql --user=ispbillingone --password=$password --database=ispbillingone  < $acct_stop_file");
+            shell_exec("rm $acct_stop_file");
+        }
+
+        return 0;
+    }
+}
--- app/Http/Controllers/SmsGatewayController.php
+++ app/Http/Controllers/SmsGatewayController.php
@@ -2,30 +2,133 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\operator;
 use App\Models\sms_gateway;
+use App\Models\sms_history;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class SmsGatewayController extends Controller
 {
     /**
+     * Get the SMS Gateway for the operator
+     *
+     * @param  \App\Models\operator  $operator
+     * @return  \App\Models\sms_gateway
+     */
+
+    public function getSmsGw(operator $operator)
+    {
+
+        $cache_key = 'sms_gw_' . $operator->id;
+
+        $ttl = 600;
+
+        return Cache::remember($cache_key, $ttl, function () use ($operator) {
+
+            //use own Gateway
+            $count = sms_gateway::where('operator_id', $operator->id)->count();
+            if ($count) {
+                return sms_gateway::where('operator_id', $operator->id)->first();
+            }
+            return sms_gateway::make([
+                'id' => 0,
+                'operator_id' => 0,
+                'provider_name' => 'Demo',
+                'unit_price' => '0.0',
+                'from_number' => '01751000000',
+            ]);
+        });
+    }
+
+
+
+    /**
+     * send sms to number
+     *
+     * @param  \App\Models\operator  $operator
+     * @param string $mobile_number
+     * @param string $message
+     * @param int $customer_id
+     *
+     * @return mixed
+     */
+
+    public function sendSms(operator $operator, string $mobile_number, string $message, int $customer_id = 0)
+    {
+
+        // Disabled Messages
+        if (strlen($message) < 5) {
+            return 0;
+        }
+
+        //sms gw
+        $sms_gateway = $this->getSmsGw($operator);
+
+        //sms_history
+        $sms_history = new sms_history();
+        $sms_history->operator_id = $operator->id;
+        $sms_history->customer_id = $customer_id;
+        $sms_history->sms_gateway_id = $sms_gateway->id;
+        $sms_history->from_number = $sms_gateway->from_number;
+        $sms_history->to_number = $mobile_number;
+        $sms_history->status_text = 'Pending';
+        $sms_history->sms_body = $message;
+        $sms_history->date = date(config('app.date_format'));
+        $sms_history->week = date(config('app.week_format'));
+        $sms_history->month = date(config('app.month_format'));
+        $sms_history->year = date(config('app.year_format'));
+        $sms_history->save();
+
+        // demo sms gateway
+        if ($sms_gateway->id == 0) {
+            return $sms_history->id;
+        }
+
+        // demo mgid
+        if ($operator->mgid == config('consumer.demo_gid')) {
+            $sms_history->status_text = 'Successful';
+            $sms_history->save();
+            return $sms_history->id;
+        }
+
+        return $sms_history->id;
+    }
+
+
+
+    /**
      * Display a listing of the resource.
      *
+     * @param \Illuminate\Http\Request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($request->user()->role == 'developer') {
+            $sms_gateways = sms_gateway::all();
+            return view('admins.developer.sms-gateways', [
+                'sms_gateways' => $sms_gateways,
+            ]);
+        }
     }
+
 
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        if ($request->user()->role == 'developer') {
+            $operators = operator::all();
+            return view('admins.developer.sms-gateway-create', [
+                'operators' => $operators,
+            ]);
+        }
     }
+
 
     /**
      * Store a newly created resource in storage.
@@ -35,19 +138,26 @@ class SmsGatewayController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $cache_key = 'sms_gw_' . $request->operator_id;
+        if (Cache::has($cache_key)) {
+            Cache::forget($cache_key);
+        }
+        $sms_gateway = new sms_gateway();
+        $sms_gateway->operator_id = $request->operator_id;
+        $sms_gateway->provider_name = $request->provider_name;
+        $sms_gateway->username = $request->username;
+        $sms_gateway->password = $request->password;
+        $sms_gateway->from_number = $request->from_number;
+        $sms_gateway->post_url = $request->post_url;
+        $sms_gateway->delivery_report_url = $request->delivery_report_url;
+        $sms_gateway->balance_check_url = $request->balance_check_url;
+        $sms_gateway->unit_price = $request->unit_price;
+        $sms_gateway->saleable = $request->saleable;
+        $sms_gateway->save();
+        return redirect()->route('sms_gateways.index')->with('success', 'SMS gateway has been added successfully!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\sms_gateway  $sms_gateway
-     * @return \Illuminate\Http\Response
-     */
-    public function show(sms_gateway $sms_gateway)
-    {
-        //
-    }
+
 
     /**
      * Show the form for editing the specified resource.
@@ -57,8 +167,11 @@ class SmsGatewayController extends Controller
      */
     public function edit(sms_gateway $sms_gateway)
     {
-        //
+        return view('admins.developer.sms-gateway-edit', [
+            'sms_gateway' => $sms_gateway,
+        ]);
     }
+
 
     /**
      * Update the specified resource in storage.
@@ -69,7 +182,21 @@ class SmsGatewayController extends Controller
      */
     public function update(Request $request, sms_gateway $sms_gateway)
     {
-        //
+        $cache_key = 'sms_gw_' . $sms_gateway->operator_id;
+        if (Cache::has($cache_key)) {
+            Cache::forget($cache_key);
+        }
+        $sms_gateway->username = $request->username;
+        $sms_gateway->password = $request->password;
+        $sms_gateway->from_number = $request->from_number;
+        $sms_gateway->post_url = $request->post_url;
+        $sms_gateway->delivery_report_url = $request->delivery_report_url;
+        $sms_gateway->balance_check_url = $request->balance_check_url;
+        $sms_gateway->unit_price = $request->unit_price;
+        $sms_gateway->saleable = $request->saleable;
+        $sms_gateway->save();
+
+        return redirect()->route('sms_gateways.index')->with('success', 'SMS Gateway has been updated Successfully');
     }
 
     /**
@@ -80,6 +207,11 @@ class SmsGatewayController extends Controller
      */
     public function destroy(sms_gateway $sms_gateway)
     {
-        //
+        $cache_key = 'sms_gw_' . $sms_gateway->operator_id;
+        if (Cache::has($cache_key)) {
+            Cache::forget($cache_key);
+        }
+        $sms_gateway->delete();
+        return redirect()->route('sms_gateways.index')->with('success', 'SMS Gateway has been deleted successfully');
     }
 }
